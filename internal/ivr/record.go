2866f7afa7b67813664364a51bb4bb5b39c7d998
rsh <randrianirinahernandezstephan@gmail.com>
2025-11-11 10:11:25 +0300

    "feat: - Passing filename as pointer parameter        - firstRecord and secondRecord func        - listeningRecord fun"

--- internal/ivr/record.go
+++ internal/ivr/record.go
@@ -9,44 +9,65 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-func RecordingRequest(ctx context.Context, ch *ari.ChannelHandle) error {
+func RecordingRequest(filename *string) ChannelHandler {
+	return func(ctx context.Context, ch *ari.ChannelHandle) error {
+		// The default directory for recordings is /var/spool/asterisk/recording/
+		//
+		// filename := fmt.Sprintf("msg_%s_%d", ch.ID(), time.Now().Unix())
 
-	// The default directory for recordings is /var/spool/asterisk/recording/
-	filename := fmt.Sprintf("msg_%s_%d", ch.ID(), time.Now().Unix())
+		rec, err := ch.Record(*filename, &ari.RecordingOptions{
+			Format:      "wav",
+			MaxDuration: 120 * time.Second,
+			MaxSilence:  5 * time.Second,
+			Exists:      "overwrite",
+			Beep:        true,
+			Terminate:   "#"},
+		)
 
-	rec, err := ch.Record(filename, &ari.RecordingOptions{
-		Format:      "wav",
-		MaxDuration: 120 * time.Second,
-		MaxSilence:  5 * time.Second,
-		Exists:      "overwrite",
-		Beep:        true,
-		Terminate:   "#"},
-	)
+		go func() {
+			<-ctx.Done()
+			rec.Stop()
+			log.Info("Context cancelled, recording stopped.", "filename", *filename)
 
-	go func() error {
-		<-ctx.Done()
-		rec.Stop()
-		return ctx.Err()
+		}()
 
-	}()
-
-	if err != nil {
-		log.Errorf("Failed to start recording: %v", err)
-		return err
+		if err != nil {
+			log.Errorf("Failed to start recording: %v", err)
+			return err
+		}
+		log.Info("Started recording", "filename", filename)
+		chanRec := rec.Subscribe("RecordingFinished")
+		<-chanRec.Events()
+		log.Info("Recording finished", "filename", filename)
+		log.Info("The program should stop now!")
+		return nil
 	}
-	log.Info("Started recording", "filename", filename)
-	chanRec := rec.Subscribe("RecordingFinished")
-	<-chanRec.Events()
-	log.Info("Recording finished", "filename", filename)
-	log.Info("The program should stop now!")
-	return nil
-
 }
 
-func firstRecord() map[string]ChannelHandler {
+func ListentRecording(filename *string) ChannelHandler {
+	return func(ctx context.Context, ch *ari.ChannelHandle) error {
+		log.Info("Playing recording", "filename", *filename)
+		plabackId := fmt.Sprintf("recording:%s", *filename)
+		mediaURI := fmt.Sprintf("recording:%s", *filename)
+		_, err := ch.Play(plabackId, mediaURI)
+		return err
+	}
+}
+
+func firstRecord(filename *string) map[string]ChannelHandler {
 	return map[string]ChannelHandler{
-		"1":       RecordingRequest,
-		"2":       StopCall,
+		"1":       RecordingRequest(filename),
+		"0":       StopCall,
 		"default": DoNothing,
 	}
+}
+
+func secondRecord(filename *string) map[string]ChannelHandler {
+	return map[string]ChannelHandler{
+		"1":       RecordingRequest(filename),
+		"2":       ListentRecording(filename),
+		"0":       StopCall,
+		"default": DoNothing,
+	}
+
 }
