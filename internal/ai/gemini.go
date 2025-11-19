ba248c8ab62038467eea99d133faee7ef1930aea
rsh <randrianirinahernandezstephan@gmail.com>
2025-11-19 17:19:19 +0300

    feat: AI features, Gemini Client

---
+++ internal/ai/gemini.go
@@ -0,0 +1,43 @@
+package ai
+
+import (
+	"context"
+	// "encoding/json"
+
+	"os"
+
+	"github.com/charmbracelet/log"
+	"google.golang.org/genai"
+)
+
+func GeminiClient(ctx context.Context) (*genai.Client, error) {
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  os.Getenv("GEMINI_API_KEY"),
+		Backend: genai.BackendGeminiAPI,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func GeminiChatClient(ctx context.Context, client *genai.Client) (*genai.Chat, error) {
+
+	chat, err := client.Chats.Create(ctx, "gemini-2.5-flash", nil, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	return chat, nil
+}
+
+func SendGeminiMessage(ctx context.Context, chat *genai.Chat, message string) (string, error) {
+
+	result, err := chat.SendMessage(ctx, genai.Part{Text: message})
+	if err != nil {
+		return "", err
+	}
+	log.Info("Gemini response:", "response", result.Text())
+	return result.Text(), nil
+
+}
