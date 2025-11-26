b6ea72fcce9fc6436a50a80831270be7f15e92d5
rsh <randrianirinahernandezstephan@gmail.com>
2025-11-26 14:38:29 +0300

    feat: handler for the creation the externalmedia channel

--- internal/ivr/handler.go
+++ internal/ivr/handler.go
@@ -4,14 +4,19 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	// "os"
 	"time"
 
 	"ari/internal/ai"
 	"ari/internal/stt"
 
+	// "ari/internal/tts"
+	"ari/internal/externalmedia"
+
 	"github.com/CyCoreSystems/ari/v5"
 	"github.com/charmbracelet/log"
-	apiInterfaces "github.com/deepgram/deepgram-go-sdk/pkg/api/prerecorded/v1/interfaces"
+	apiPrerecordedInterfaces "github.com/deepgram/deepgram-go-sdk/pkg/api/prerecorded/v1/interfaces"
+	apiSpeakResponseInterfaces "github.com/deepgram/deepgram-go-sdk/pkg/api/speak/v1/rest/interfaces"
 )
 
 type ChannelHandler func(ctx context.Context, h *ari.ChannelHandle) error
@@ -66,7 +71,8 @@ func callHandl(mainCtx context.Context, subCtx context.Context, subCancel contex
 
 	recFilename := fmt.Sprintf("msg_%s_%d", h.ID(), time.Now().Unix())
 	DTMFHandl(mainCtx, "sound:welcome-ari", client, h, firstRecord(&recFilename)) //First record wiht welcome message
-	var resBody apiInterfaces.PreRecordedResponse
+	var recResBody apiPrerecordedInterfaces.PreRecordedResponse
+	var speakResBody apiSpeakResponseInterfaces.SpeakResponse
 
 	for {
 		select {
@@ -75,7 +81,7 @@ func callHandl(mainCtx context.Context, subCtx context.Context, subCancel contex
 			return
 
 		default:
-			DTMFHandl(mainCtx, "sound:rick-astley", client, h, secondRecord(&recFilename, &resBody)) //Second record with listen option and another message
+			DTMFHandl(mainCtx, "sound:rick-astley", client, h, secondRecord(&recFilename, &recResBody, &speakResBody)) //Second record with listen option and another message
 		}
 	}
 
@@ -93,7 +99,7 @@ func DoNothing(ctx context.Context, h *ari.ChannelHandle) error {
 	return nil
 }
 
-func ValidateSend(filename *string, resBody *apiInterfaces.PreRecordedResponse) ChannelHandler {
+func ValidateSend(filename *string, recResBody *apiPrerecordedInterfaces.PreRecordedResponse, speakResBody *apiSpeakResponseInterfaces.SpeakResponse) ChannelHandler {
 	return func(ctx context.Context, h *ari.ChannelHandle) error {
 		//Get the recording bite audio
 		audio, err := downloadRecordingFromARI(ctx, filename)
@@ -103,17 +109,17 @@ func ValidateSend(filename *string, resBody *apiInterfaces.PreRecordedResponse) 
 		}
 		reader := bytes.NewReader(audio)
 		//Send to Deepgram STT
-		err = stt.DgSendPreRecorded(ctx, reader, resBody)
+		err = stt.DgSendPreRecorded(ctx, reader, recResBody)
 		if err != nil {
 			return err
 		}
 
 		//Get the transcription result
-		fmt.Printf("Request ID: %s\n", resBody.RequestID)
-		if resBody.Results != nil &&
-			len(resBody.Results.Channels) > 0 &&
-			len(resBody.Results.Channels[0].Alternatives) > 0 {
-			transcript := resBody.Results.Channels[0].Alternatives[0].Transcript
+		fmt.Printf("Request ID: %s\n", recResBody.RequestID)
+		if recResBody.Results != nil &&
+			len(recResBody.Results.Channels) > 0 &&
+			len(recResBody.Results.Channels[0].Alternatives) > 0 {
+			transcript := recResBody.Results.Channels[0].Alternatives[0].Transcript
 			fmt.Println("Transcription:", transcript)
 
 			//Gemini Part
@@ -125,6 +131,7 @@ func ValidateSend(filename *string, resBody *apiInterfaces.PreRecordedResponse) 
 			gemChat, err := ai.GeminiChatClient(ctx, gemClient) //Create Gemini chat session
 			if err != nil {
 				return err
+
 			}
 			log.Info("Gemini chat session created")
 
@@ -135,6 +142,44 @@ func ValidateSend(filename *string, resBody *apiInterfaces.PreRecordedResponse) 
 				return err
 			}
 			log.Info("Gemini response received", "response", reqResult)
+			// ---External Media Part---
+
+			// params := externalmedia.ExternalMediaParams{
+			// 	ARIBaseURL: os.Getenv("ARI_URL"),
+			// 	Username:   os.Getenv("ARI_USERNAME"),
+			// 	Password:   os.Getenv("ARI_PASSWORD"),
+			//
+			// 	AppName: os.Getenv("ARI_APPLICATION_NAME"),
+			// 	HostIP:  os.Getenv("ARI_IP"),
+			// 	Port:    4002,
+			// 	Format:  "slin16",
+			// }
+			params := externalmedia.ExternalMediaParams{
+				ARIBaseURL: "http://192.168.122.113:8088",
+				Username:   "ari_user",
+				Password:   "password",
+
+				AppName: "app",
+				HostIP:  "192.168.122.113",
+				Port:    4002,
+				Format:  "slin16",
+			}
+			result, err := externalmedia.CreateExternalMedia(params)
+			if err != nil {
+				log.Fatal("External Media creation failed", "error", err)
+			}
+			fmt.Println("External Media Channel Created:", result)
+
+			// ---------------------TTS Part (Disabled)---------------------
+			// var ttsKeys []string
+			// eror := tts.GetDgTTS(ctx, reqResult, ttsKeys, speakResBody)
+			//
+			// if eror != nil {
+			// 	log.Error("Error in TTS:", "error", eror)
+			// 	return eror
+			// }
+			// speakResBody.RequestID
+			// // something wrong here
 		}
 
 		return nil
