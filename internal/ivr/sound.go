96bc21545bd10f98e0b32e0eca33b8e83425e483
rsh <randrianirinahernandezstephan@gmail.com>
2025-12-22 16:54:20 +0300

    feat: improvement comment

--- internal/ivr/sound.go
+++ internal/ivr/sound.go
@@ -24,6 +24,7 @@ func PlaySound(ctx context.Context, ch *ari.ChannelHandle, soundURI string) erro
 }
 
 func promptSound(ctx context.Context, ch *ari.ChannelHandle, soundURI string) (*play.Result, error) {
+	// PERF: put the digit for the dtmf signal as parameter and the number of replay too
 	for {
 		select {
 		case <-ctx.Done():
